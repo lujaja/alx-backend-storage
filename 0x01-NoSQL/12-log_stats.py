a65f6a3b890fce10295988a1833752886d5fffd8
lujaja <jarzcyber@gmail.com>
2024-05-14 11:37:42 +0300

    stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,25 +5,29 @@ Log stats
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs
-collection = db.nginx
-total_logs = collection.count_documents({})
+if __name__ == '__main__':
+    """
+    provide some stats about nginx logs
+    """
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    collection = db.nginx
+    total_logs = collection.count_documents({})
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-method_counts = {
-    method: collection.count_documents(
-        {"method": method}
-    ) for method in methods
-}
+    method_counts = {
+        method: collection.count_documents(
+            {"method": method}
+        ) for method in methods
+    }
 
-status_check_count = collection.count_documents(
-    {"method": "GET", "path": "/status"}
-)
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-print(f"{total_logs} logs")
-print("Methods:")
-for method in methods:
-    print(f"\tmethod {method}: {method_counts[method]}")
-print(f"{status_check_count} status check")
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check_count} status check")
