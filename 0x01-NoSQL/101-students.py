ee70f725de731d621c9c3caaa714bef0fadc518a
lujaja <jarzcyber@gmail.com>
2024-05-14 11:59:19 +0300

    average

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python3
+"""
+Top Students
+"""
 
+
+def top_students(mongo_collection):
+    """
+    Function - top_students
+
+    Attributes:
+        mongo_collection (pymongo.collection.Collection):
+
+    Returns:
+        list
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
